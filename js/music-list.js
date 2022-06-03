a915168832f55439a89ff8d7edb5676c2599581d
Jalaj Porwal <90232116+jalajporwal@users.noreply.github.com>
2022-06-03 17:09:54 +0530

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,68 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Oo Antava Oo Oo Antava",
+    artist: "Indravathi Chauhan",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Haaye Chaka Chak Chaka Chak Hai Tu",
+    artist: "Shreya Ghoshal",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Pehli Mohabbat",
+    artist: "Darshan Raval",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Tareefan Reprise Veere-Di-Wedding",
+    artist: "Lisa Mishra and Qaran",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Ek Ladki Ko Dekha Toh Aisa Laga",
+    artist: "Darshan Raval",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Srivalli - Pushpa",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Khoya Sa",
+    artist: "K A S Y A P",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Tere Bina",
+    artist: "Zaeden",
+    img: "music-8",
+    src: "music-8"
+  },
+  // Like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
